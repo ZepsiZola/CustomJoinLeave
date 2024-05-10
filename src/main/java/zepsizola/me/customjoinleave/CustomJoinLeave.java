fc9c9c66b78efdedc5804c6d5613d704ce4ab964
c21067326 <mcbrided@cardiff.ac.uk>
2024-05-10 19:18:15 +0100

    Plugin works for the main part now. Has MiniMessage and PAPI support.

--- src/main/java/zepsizola/me/customjoinleave/CustomJoinLeave.java
+++ src/main/java/zepsizola/me/customjoinleave/CustomJoinLeave.java
@@ -1,17 +1,59 @@
 package zepsizola.me.customjoinleave;
 
+import me.clip.placeholderapi.PlaceholderAPI;
+import net.kyori.adventure.text.Component;
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import net.kyori.adventure.text.minimessage.MiniMessage;
 
-public final class CustomJoinLeave extends JavaPlugin {
+import java.io.File;
+
+public class CustomJoinLeave extends JavaPlugin implements Listener {
+
+    private boolean PAPIEnabled = false;
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        // Check if PlaceholderAPI is available
+        if (Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null) {
+            PAPIEnabled = true;
+            getLogger().info("PlaceholderAPI enabled.");
+        } else {
+            getLogger().info("PlaceholderAPI not found. PAPI support disabled.");
+        }
 
+        this.getServer().getPluginManager().registerEvents(this, this);
+
+        this.saveDefaultConfig();
+        File configFile = new File(getDataFolder(), "config.yml");
+        if (!configFile.exists()) {
+            saveResource("config.yml", false);
+        }
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        String message = getConfig().getString("joinMessage", "<yellow>%player_name% <yellow>joined the game");
+        if (PAPIEnabled) {
+            message = PlaceholderAPI.setPlaceholders(event.getPlayer(), message);
+        }
+
+        Component joinMessage = MiniMessage.miniMessage().deserialize(message);
+        event.joinMessage(joinMessage);
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        String message = getConfig().getString("leaveMessage", "<yellow>%player_name% <yellow>left the game");
+        if (PAPIEnabled) {
+            message = PlaceholderAPI.setPlaceholders(event.getPlayer(), message);
+        }
+
+        Component quitMessage = MiniMessage.miniMessage().deserialize(message);
+        event.quitMessage(quitMessage);
     }
 }
