79cbf3c719299841942cb8922ea922462c133499
c21067326 <mcbrided@cardiff.ac.uk>
2024-05-10 17:06:51 +0100

    Initial Commit

---
+++ src/main/java/zepsizola/me/customjoinleave/CustomJoinLeave.java
@@ -0,0 +1,17 @@
+package zepsizola.me.customjoinleave;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CustomJoinLeave extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
